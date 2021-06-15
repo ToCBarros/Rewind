a415ab7f692a0a72df6e062ae66883cce65df75d
Tomas Barros <aluno20782@ipt.pt>
2021-06-15 01:33:54 +0100

    Criação da base de dados

---
+++ Rewind/Rewind/Migrations/20210615002105_Inicio.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rewind.Migrations
+{
+    public partial class Inicio : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Estudios",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Estudio = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Pais = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Estudios", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Utilizadores",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Utilizador = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Utilizadores", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Sinopse = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Episodios = table.Column<int>(type: "int", nullable: false),
+                    Estado = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Ano = table.Column<int>(type: "int", nullable: false),
+                    Imagem = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EstudioID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Series_Estudios_EstudioID",
+                        column: x => x.EstudioID,
+                        principalTable: "Estudios",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comentarios",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UtilizadoresID = table.Column<int>(type: "int", nullable: false),
+                    SeriesID = table.Column<int>(type: "int", nullable: false),
+                    estado = table.Column<bool>(type: "bit", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Comentario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Estrelas = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comentarios", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_Series_SeriesID",
+                        column: x => x.SeriesID,
+                        principalTable: "Series",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Comentarios_Utilizadores_UtilizadoresID",
+                        column: x => x.UtilizadoresID,
+                        principalTable: "Utilizadores",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_SeriesID",
+                table: "Comentarios",
+                column: "SeriesID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comentarios_UtilizadoresID",
+                table: "Comentarios",
+                column: "UtilizadoresID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Series_EstudioID",
+                table: "Series",
+                column: "EstudioID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comentarios");
+
+            migrationBuilder.DropTable(
+                name: "Series");
+
+            migrationBuilder.DropTable(
+                name: "Utilizadores");
+
+            migrationBuilder.DropTable(
+                name: "Estudios");
+        }
+    }
+}
